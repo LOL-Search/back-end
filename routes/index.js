8a7b9ad3f1ff0d6dbc63d52b2169b50b649fc4aa
JaeYoung <kimjaeyoung650@gmail.com>
2024-12-17 01:48:19 +0900

    [feat] 테스트 토큰 기능 추가

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const token = require('../utils/jwt');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const payload = { id: 1 };
+  const jwt = token.generateToken(payload);
+  res.header('Authorization', jwt).json({ token : jwt });
+  // res.render('index', { title: 'Express' })
 });
 
 module.exports = router;

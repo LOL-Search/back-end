0b0de09108828a172ecec2ce3de0f5624df33de4
KangJuHyeong <hjk7935@naver.com>
2024-12-17 16:30:12 +0900

    ec2 test

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,17 @@ var router = express.Router();
 const authController = require('../controllers/authController');
 
 router.post('/login', authController.googleLogin);
+router.get('/test', (req, res) => {
+    // 테스트용 JSON 응답
+    const testData = {
+        message: "This is a test response",
+        status: "success",
+        data: {
+            user: "John Doe",
+            age: 30
+        }
+    };
 
+    res.json(testData);  // JSON 형식으로 응답
+});
 module.exports = router;

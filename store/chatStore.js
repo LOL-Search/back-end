1e3cf8c2b05a65dac291efc6d6a51d313b6d9a81
JaeYoung <kimjaeyoung650@gmail.com>
2024-12-19 15:48:36 +0900

    [refactor] 소캣 통신 디렉토리 수정

---
+++ store/chatStore.js
@@ -0,0 +1,21 @@
+const db = require('../config/db');
+
+class ChatStore {
+  async saveChat(data) {
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+
+  async saveUser(data) {
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+}
+
+module.exports = new ChatStore();

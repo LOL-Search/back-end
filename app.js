ff61952c2395a754d12c01344523352c92010427
JaeYoung <kimjaeyoung650@gmail.com>
2024-12-20 20:52:04 +0900

    [fix] cors 오타 수정

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
-  origin: ['*'],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
